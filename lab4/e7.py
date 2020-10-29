b87fea3f6fd56244b7fa990b9e61d9ff2f37f128
Daemond <daemondeal@gmail.com>
2020-10-29 15:38:48 +0100

    Finito l'esercizio 7 del lab4

--- lab4/e7.py
+++ lab4/e7.py
@@ -1,28 +1,66 @@
 import random
 import math
 
-print("NIM")
-
 balls = random.randint(10, 100)
 ai_level = random.randint(0, 1)
-
-print("Numero Biglie: ")
-
-
 turn = random.randint(0, 1)
+
+print("Numero Biglie:", balls)
+if ai_level == 0:
+    print("Strategia AI: Casuale")
+else:
+    print("Strategia AI: Ottimale")
+
+input("Premi invio per iniziare: ")
+
+print()
+
+if turn == 0:
+    print("Inizia il giocatore!")
+else:
+    print("Inizia la AI!")
+
+
 turn_num = 1
 
 while balls > 0:
-	print("Turno", turn_num)
-	print("Biglie rimanenti:", balls)
-	if turn == 0: # Player Turn
-		pass
-	else: # AI Turn
-		print("Turno del computer.")
+    max_move = math.ceil(balls / 2)
+    print("Turno", turn_num)
+    print("Biglie rimanenti:", balls)
+    print("Mossa massima:", max_move)
 
-		if ai_level == 0: # dumb
-			move = random.randint(1, math.floor(balls / 2))
-		else: # Smart
+    if turn == 0: # Player Turn
+        move = int(input(f"Inserisci il numero di biglie (Tra 1 e {max_move}): "))
+        while move > max_move or move < 0:
+            print("Numero invalido.")
+            move = int(input((f"Inserisci il numero di biglie (Tra 1 e {max_move}): ")))
 
+    else: # AI Turn
+        if ai_level == 0: # dumb
+            move = random.randint(1, max_move)
+        else: # Smart
+            nearest_power = int(2 ** math.floor(math.log2(balls))) - 1
+            distance = balls - nearest_power
 
-	print()
+            if distance > 0:
+                move = distance
+            else:
+                move = random.randint(1, max_move)
+
+        print("Turno del computer...")
+        print(f"Prende {move} biglie!")
+
+    balls -= move
+    if balls > 0:
+        turn_num += 1
+        if turn == 1:
+            turn = 0
+        else:
+            turn = 1
+    print()
+
+print("Partita finita! Il vincitore è ", end="")
+if turn == 1:
+    print("il giocatore!")
+else:
+    print("il computer!")
