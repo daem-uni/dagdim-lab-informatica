3f237868da4236d67bdd5ecfa79ce65c9a9b3d46
Daemond <daemondeal@gmail.com>
2020-10-30 15:46:01 +0100

    Fixato errore in lab4/e5.py

--- lab4/e5.py
+++ lab4/e5.py
@@ -4,7 +4,7 @@ for i in range(2, n + 1):
     prime = True
 
     if i != 2 and i != 3:
-        for j in range(2, int(n ** .5) + 1):
+        for j in range(2, int(i ** .5) + 1):
             if i % j == 0:
                 prime = False
 
