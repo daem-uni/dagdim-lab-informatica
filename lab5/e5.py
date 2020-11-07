93d04e0a740b44edb69c891386ba980a31974993
Daemond <daemondeal@gmail.com>
2020-11-07 17:47:23 +0100

    Fixato lab5/es5.py

--- lab5/e5.py
+++ lab5/e5.py
@@ -1,10 +1,10 @@
-A = 0.1
-B = 0.01
-C = 0.01
-D = 0.00002
+A = float(input("Inserire il valore A: ")) # 0.1
+B = float(input("Inserire il valore B: ")) # 0.01
+C = float(input("Inserire il valore C: ")) # 0.01
+D = float(input("Inserire il valore D: ")) # 0.00002
 
-prey_old = 1000
-pred_old = 20
+prey_old = int(input("Inserire il valore di prede iniziali: ")) # 1000
+pred_old = int(input("Inserire il numero di predatori iniziale: ")) # 20
 
 generations = int(input("Inserisci il numero di generazioni da simulare: "))
 
@@ -12,8 +12,8 @@ print("La popolazione iniziale di prede è", prey_old)
 print("La popolazione iniziale di predatori è", pred_old)
 
 for t in range(generations):
-    prey_new = prey_old * (1 + A - B * pred_old)
-    pred_new = pred_old * (1 - C + D * prey_old)
+    prey_new = int(prey_old * (1 + A - B * pred_old))
+    pred_new = int(pred_old * (1 - C + D * prey_old))
 
     print()
     print("Generazione", t + 1)
