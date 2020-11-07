2da5eb0b067d3ab0ac45356feb92961718656bef
Daemond <daemondeal@gmail.com>
2020-11-07 18:51:09 +0100

    Fixato lab5/e7.py

--- lab5/e3.py
+++ lab5/e3.py
@@ -4,7 +4,6 @@ while n != 1:
     factor = 2
     while n % factor != 0:
         factor += 1
-
+    
     n = n // factor
     print(factor)
-    
--- lab5/e7.py
+++ lab5/e7.py
@@ -5,12 +5,18 @@ tickets_to_buy = 0
 buyers = 0
 
 while tickets_left > 0:
-    tickets_to_buy = int(input("Inserire numero biglietti da acquistare: "))
+    tickets_to_buy = int(input("Inserire numero biglietti da acquistare (Massimo 4): "))    
+    while tickets_to_buy < 1 or tickets_to_buy > 4:
+        print("Numero Invalido. Dev'essere minimo 1 e massimo 4.")
+        tickets_to_buy = int(input("Inserire numero biglietti da acquistare (Massimo 4): "))    
+        
     print("Acquisto avvenuto con successo!")
     tickets_left -= tickets_to_buy
     buyers += 1
     
     print(tickets_left, "biglietti rimasti.")
     print()
+
+    tickets_to_buy = 0
     
 print("Biglietti esauriti. Acquirenti totali:", buyers)
