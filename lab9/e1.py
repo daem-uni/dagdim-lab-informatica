32b524a5c69b7b4a5677e8060651583de5f35948
Daemond <daemondeal@gmail.com>
2020-12-03 16:20:46 +0100

    Fixato lab9/e1.py

--- lab9/e1.py
+++ lab9/e1.py
@@ -8,7 +8,7 @@ def swapFirstAndLast(array):
 def rightShift(array):
     tmp = array[-1]
     for i in range(len(array)-1, 0, -1):
-        array[i-1] = array[i]
+        array[i] = array[i-1]
     array[0] = tmp
 
     return array
